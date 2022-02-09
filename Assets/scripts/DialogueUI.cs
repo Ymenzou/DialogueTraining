b2f5bd6f534f4b3c9b57c69153fd05aa37c0459f
Ymenzou <86317017+Ymenzou@users.noreply.github.com>
2022-02-09 23:48:07 +0100

    add

---
+++ Assets/scripts/DialogueUI.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DialogueUI : MonoBehaviour
+{
+    [SerializeField] private TMP_Text textLabel;
+
+    private void Start()
+    {
+        GetComponent<TypewriterEffect>().Run("This is a bit of text!\nhello.",textLabel);
+    }
+    
+}
